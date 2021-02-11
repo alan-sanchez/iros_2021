3037f0ecfaf71f05f2080aa9b36a550b4c42b498
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-02-10 20:11:14 -0800

    Modify code to compute dosage for 1 sensor

--- src/uv_meter_wave/uv_meter_wave.ino
+++ src/uv_meter_wave/uv_meter_wave.ino
@@ -1,42 +1,43 @@
 
-int UVOUT = A0; //Output from the sensor
+//int sense_0 = A0; //Output from the sensor
+int last_time = 0;
+int delta_t;
+float V_conversion = 1.22;
+float UV_Dose_0, UV_Dose_1, UV_Dose_2;
 
-void setup()
-{
+void setup(){
   Serial.begin(9600);
-  pinMode(UVOUT, INPUT);
-
 }
 
-void loop()
-{
-//  float Vo = 5.0*analogRead(UVOUT)/1023.0;
-//  float Vo_d = analogRead(UVOUT);
-//  float Vo_map = map(Vo_d, 0, 755, 0, 1023);
-//  float Di = (5.0*Vo_map/1023.0)/4.3;
-    
-  float Di = 5.0*analogRead(UVOUT)/1023.0/1.22;
+void loop(){
+//  Serial.print(millis());
+//  Serial.print(", ");
+  delta_t = millis() - last_time;
 
-  float uvIntensity = mapfloat(Di, 0.0, 1.0, 0.0, 9.0); //Convert the voltage to a UV intensity level
-  //  Serial.print(millis());
+  float Di_0 = diode_current(analogRead(A0));
 
-  //  Serial.print(" / UV Intensity (mW/cm^2): ");
-//  Serial.print(Vo_d);
+  float Ir_0 = mapfloat(Di_0, 0.0, 1.0, 0.0, 9.0); //Convert the voltage to a UV intensity level
+
+  UV_Dose_0 += Ir_0 * delta_t/1000.0;
+//  float uvDosage_0 = uvIntensity_0 *=
+//  Serial.print(delta_t);
 //  Serial.print(", ");
-//  Serial.print(Vo_map);
-//  Serial.print(", ");
-//  Serial.print(Di);
-//  Serial.print(", ");
-  Serial.print(uvIntensity);
+  Serial.print(Ir_0);
   Serial.print(", ");
-  Serial.println(Di);
-
-  delay(200);
+  Serial.println(UV_Dose_0);
+  last_time = millis();
+  delay(1000);
 }
 
 
+float diode_current(float A_in){
+  return 5.0 * A_in / 1023.0 / V_conversion;
+}
 
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max){
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+//float dosage(float Ir, T){
+//  return
+//}
