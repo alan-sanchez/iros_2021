fee9e2c5255f37d17adf91f71d058d4647af0697
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-02-10 20:20:25 -0800

    Added comments to code

--- src/uv_meter_wave/uv_meter_wave.ino
+++ src/uv_meter_wave/uv_meter_wave.ino
@@ -1,31 +1,37 @@
 
-//int sense_0 = A0; //Output from the sensor
-int last_time = 0;
-int delta_t;
+// Delcare all constants and variables
+volatile unsigned long last_time = 0;
+volatile unsigned long delta_t;
+volatile float UV_Dose_0, UV_Dose_1, UV_Dose_2;
 float V_conversion = 1.22;
-float UV_Dose_0, UV_Dose_1, UV_Dose_2;
 
 void setup(){
+  // Set baud rate
   Serial.begin(9600);
 }
 
 void loop(){
-//  Serial.print(millis());
-//  Serial.print(", ");
+  // Compute actual polling time difference
   delta_t = millis() - last_time;
 
+  // Compute the diode current of the sensor with diode_current function
   float Di_0 = diode_current(analogRead(A0));
 
+  // Compute Irradaince at sensor with mapfloat function
   float Ir_0 = mapfloat(Di_0, 0.0, 1.0, 0.0, 9.0); //Convert the voltage to a UV intensity level
 
+  // Update UV dosage at sensor
   UV_Dose_0 += Ir_0 * delta_t/1000.0;
-//  float uvDosage_0 = uvIntensity_0 *=
-//  Serial.print(delta_t);
-//  Serial.print(", ");
+
+  // Serial print the irradiance and uv dosage at sensor
   Serial.print(Ir_0);
   Serial.print(", ");
   Serial.println(UV_Dose_0);
+
+  // update the last measured time before the time delay
   last_time = millis();
+
+  // Add delay
   delay(1000);
 }
 
@@ -37,7 +43,3 @@ float diode_current(float A_in){
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max){
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
-//float dosage(float Ir, T){
-//  return
-//}
