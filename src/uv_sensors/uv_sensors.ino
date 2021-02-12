f6c6caba69ebc3ed7312901cff2b72af33117fae
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-02-11 23:45:21 -0800

    Init commit of ino file of all the sensors working together

---
+++ src/uv_sensors/uv_sensors.ino
@@ -0,0 +1,47 @@
+
+// Delcare all constants and variables
+volatile unsigned long last_time = 0;
+volatile unsigned long delta_t;
+volatile float UV_Dose_0, UV_Dose_1, UV_Dose_2, 
+               UV_Dose_3;
+float V_conversion = 1.15;
+
+void setup(){
+  // Set baud rate
+  Serial.begin(9600);
+}
+
+void loop(){
+  // Compute actual polling time difference
+  delta_t = millis() - last_time;
+
+  // Compute the diode current of the sensors with diode_current function
+  float Di_0 = diode_current(analogRead(A0));
+  
+
+  // Compute Irradaince at sensor with mapfloat function
+  float Ir_0 = mapfloat(Di_0, 0.0, 1.0, 0.0, 9.0); //Convert the voltage to a UV intensity level
+
+  // Update UV dosage at sensor
+  UV_Dose_0 += Ir_0 * delta_t/1000.0;
+
+  // Serial print the irradiance and uv dosage at sensor
+  Serial.print(Ir_0);
+  Serial.print(", ");
+  Serial.println(UV_Dose_0);
+
+  // update the last measured time before the time delay
+  last_time = millis();
+
+  // Add delay
+  delay(500);
+}
+
+
+float diode_current(float A_in){
+  return 5.0 * A_in / 1023.0 / V_conversion;
+}
+
+float mapfloat(float x, float in_min, float in_max, float out_min, float out_max){
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
