410b599fc6a264fa9674488847b1b549e448aa4b
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-02-22 11:47:51 -0800

    Removed unused data

--- src/python_scripts/accumulation.py
+++ src/python_scripts/accumulation.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import rospy
 
@@ -17,40 +17,13 @@ class Accumulation:
 		self.min_dosage = None
 
 		# import polynomial model from the best_fit python script
-		self.model = fit(3, plotter = False)
+		self.model = fit(15, plotter = False)
 		self.req_dosage = 1
-
-		self.flag = 1
 
 	def UV_dosage_callback(self,msg):
 		self.req_dosage = msg.data
 		self.convolution_technique()
 
-	def integral_technique(self):
-		## Compute area under the polynomial model. The area represents the total
-		## surface irradiance from the flashlight.
-		x = np.linspace(0,10,100)
-		y = self.model(x)
-		Irradiance = np.trapz(y, dx = x[1]) * 2
-		# print(Irradiance)
-
-		## The diameter of the lit surface is 20 cm.  If the arm's max velocity is 100cm/s
-		## then a point would experience the flashlights irradiance for 0.20 seconds.
-		## Resulting in a dosage of 0.2 * Irradiance. The ratio between minimum and
-		## required dosage informs how much to reduce translational speed of the EE
-		min_dosage = Irradiance * 0.2
-		ratio = min_dosage/self.req_dosage
-
-		if ratio > 1:
-			vel = 1
-		elif ratio < .01:
-			vel = .01
-		else:
-			vel = ratio
-
-		# print(vel)
-
-		self.vel_regulator_pub.publish(vel)
 
 		# the width and height of a pixel is defined as 1 cm.
 
@@ -77,15 +50,15 @@ class Accumulation:
 				else:
 					ramped_mask[r,c] = self.model(radius_dist)
 
-		# print(sum(ramped_mask[10,:]))
+		print(sum(ramped_mask[10,5:16]))
 		irradiance = sum(ramped_mask[window/2, :])
 
-		## The diameter of the considered disinfecting surface is 20 cm. If the arm's max velocity is 11cm/s
-		## then a point would experience the flashlights irradiance for (20/14) seconds.Resulting in a dosage of
-		## 1.43 * Irradiance. The ratio between minimum and required dosage informs how much to reduce translational
+		## The diameter of the considered disinfecting surface is 10 cm. If the arm's max velocity is 100 cm/s
+		## then a point would experience the flashlights irradiance for 10/100 seconds. Resulting in a dosage of
+		## .2 * Irradiance. The ratio between minimum and required dosage informs how much to reduce translational
 		## speed of the EE.
 
-		min_dosage = irradiance * 20/11
+		min_dosage = irradiance * 10/100
 		ratio = min_dosage/self.req_dosage
 
 		if ratio > 1:
@@ -99,26 +72,26 @@ class Accumulation:
 
 		self.vel_regulator_pub.publish(vel)
 
-		# # Swipe the window across the swath, at row 5.  This is a bit special-cased for this example.
-		# for c in range(columns - window - 1):
-		# 	accumulator[0:window, c:c + window] += ramped_mask
-		#
-		# # Force it into 8 bit format and save as an image.
-		# formatted = (accumulator * 255 / np.max(accumulator)).astype('uint8')
-		# Image.fromarray(formatted).save('output.png')
-		# im = Image.fromarray(formatted)
-		# im.show()
-		#
-		# formatted = (ramped_mask * 255 / np.max(ramped_mask)).astype('uint8')
-		# im = Image.fromarray(formatted)
-		# im.show()
-		# Image.fromarray(formatted).save('ramped_mask.png')
-	
+	# def show_mask(self):
+	# 	# # Swipe the window across the swath, at row 5.  This is a bit special-cased for this example.
+	# 	# for c in range(columns - window - 1):
+	# 	# 	accumulator[0:window, c:c + window] += ramped_mask
+	# 	#
+	# 	# # Force it into 8 bit format and save as an image.
+	# 	# formatted = (accumulator * 255 / np.max(accumulator)).astype('uint8')
+	# 	# Image.fromarray(formatted).save('output.png')
+	# 	# im = Image.fromarray(formatted)
+	# 	# im.show()
+	# 	self.convolution_technique()
+	#
+	# 	formatted = (ramped_mask * 255 / np.max(ramped_mask)).astype('uint8')
+	# 	im = Image.fromarray(formatted)
+	# 	im.show()
+	# 	Image.fromarray(formatted).save('ramped_mask.png')
+	#
 
 
 if __name__ == '__main__':
 	rospy.init_node('accumulation')
 	tech = Accumulation()
-	# tech.velocity_regulator()
-	# tech.convolution_simulation()
-	rospy.spin()
+	# rospy.spin()
---
+++ src/results/file_reader.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import csv
+
+with open('ebola_sudan_d99.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        dosage = row
+
+test_list=[]
+for i in range(0, len(dosage)):
+    test_list.append(float(dosage[i]))
+
+Sensors = ['S0', 'S1', 'S2', 'S3', 'S4',
+           'S5', 'S6', 'S7', 'S8', 'S9',
+           'S10', 'S11', 'S12', 'S13', 'S14'
+            ]
+
+plt.bar(Sensors, test_list, alpha =0.5, yerr = 10, ecolor='black', capsize=2)
+plt.show()
+print(test_list)
