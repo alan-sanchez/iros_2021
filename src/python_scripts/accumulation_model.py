99175fe35a3264a1754592d72817c714f564bcc7
Alan G. Sanchez <sanchala@oregonstate.edu>
2021-02-25 21:04:22 -0800

    included disinfected array for visuals

--- src/python_scripts/accumulation_model.py
+++ src/python_scripts/accumulation_model.py
@@ -13,14 +13,13 @@ from PIL import Image
 if __name__ == '__main__':
 	# This is the array that adds up the radiation values.
 	rows = 31
-	columns = 131
+	columns = 130
 	accumulator = np.zeros((rows, columns))
+	disinfected = np.zeros((rows, columns))
 
-	model = fit(3, plotter = False)
+	model = fit(16, plotter = False)
 
-	# Set up the convolution mask.  This one is a 5 pixel wide mask of constant value (1.0).  Note that
-	# this is different from the masks used in image processing in that the elements of the mask do not
-	# have to add up to 1.
+
 	window = 21
 
 	#time_exposure is the diameter of exposed area divided by max velocity.
@@ -50,11 +49,19 @@ if __name__ == '__main__':
 		# accumulator[0:window, c:c + window] += ramped_mask
 		accumulator[5:5+window, c:c + window] += ramped_mask/ratio
 
-	# c = plt.Circle((10,10), 5, color='r', fill=False, label='UV Cutoff Diameter')
+	for r in range(rows):
+		for c in range(columns):
+			if accumulator[r,c] > 26.5:
+				disinfected[r,c] = 1
+
+			else:
+				disinfected[r,c] = 0
+
+	# c = plt.Circle((10,10), 5, color='r', fill=False, label='Considered Size of Mask')
 	# ticks = ['-10','-9','-8','-7','-6','-5','-4','-3','-2','-1','0','1','2','3','4','5','6','7','8','9','10']
 	# fig, ax = plt.subplots(1,1)
 	# im = ax.imshow(ramped_mask)
-	# fig.colorbar(im)
+	# clb = fig.colorbar(im)
 	# ax.add_patch(c)
 	# ax.set_xticks((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20))
 	# ax.set_xticklabels(labels = ticks)
@@ -62,19 +69,38 @@ if __name__ == '__main__':
 	# ax.set_yticklabels(labels = ticks)
 	# ax.set_xlabel('Distance from the Center of UV Exposed Area (cm)')
 	# ax.set_ylabel('Distance from the Center of UV Exposed Area (cm)')
-	# ax.set_title('Ramped Mask Model of UV Light Source')
+	# ax.set_title(' Mask of UV Light Source')
+	# clb.set_label('UV Irradiance (mW/cm^2)',labelpad=15)
 	# ax.legend()
-	#
-	# plt.show()
+	# plt.savefig("ramped_mask_2.png", bbox_inches='tight')
 
-	rect = patches.Rectangle((14,14), 100,2, linewidth=1, edgecolor='r', facecolor='none',label='Region')
-	fig, ax = plt.subplots(1,1)
-	im = ax.imshow(accumulator)
-	fig.colorbar(im)
-	ax.add_patch(rect)
-	ax.set_xlabel('Length of 1-D Coverage (cm)')
-	ax.set_ylabel('Height of 1-D Coverage (cm)')
-	ax.set_title('Reduction of ')
-	ax.legend()
+	ticks = [0,10,20,30]
+	rect = patches.Rectangle((13.5,14.5), 100,1, linewidth=1, edgecolor='r', facecolor='none',label='Disinfected Region')
+	fig, ax1 = plt.subplots(1,1)
+	im1 = ax1.imshow(accumulator)
+	clb1 = fig.colorbar(im1)
+	ax1.add_patch(rect)
+	ax1.set_yticks(ticks)
+	ax1.set_yticklabels(ticks[::-1])
+	ax1.set_xlabel('Length of Coverage (cm)')
+	ax1.set_ylabel('Height of Coverage (cm)')
+	ax1.set_title('Model of Spanned Mask')
+	ax1.legend()
+	clb1.set_label('UV Irradiance (mW/cm^2)',labelpad=15)
 
+
+	# im2 = ax2.imshow(disinfected)
+	# clb2 = fig.colorbar(im2)
+	# ax2.add_patch(rect)
+	# ax2.set_yticks(ticks)
+	# ax2.set_yticklabels(ticks[::-1])
+	# ax2.set_xlabel('Length of Coverage (cm)')
+	# ax2.set_ylabel('Height of Coverage (cm)')
+	# ax2.set_title('Model of Spanned Mask')
+	# ax2.legend()
+	# clb2.set_label('UV Irradiance (mW/cm^2)',labelpad=15)
+
+	plt.subplots_adjust(left = 0.08, bottom = 0.15, right = 1, top =0.45)
+
+	plt.savefig("1D_coverage_model.png", bbox_inches='tight')
 	plt.show()
